308ab74a6ebe8dc08ea9666070db53f8cfff20f2
Wesley de Bruijn <wesley.dbr@live.nl>
2025-05-09 14:26:30 +0200

    add base

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  base: "/tree-select/",
   build: {
     outDir: "dist",
     emptyOutDir: true,
